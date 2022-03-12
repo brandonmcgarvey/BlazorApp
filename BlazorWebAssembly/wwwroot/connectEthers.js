dcfd89b381241f4e19547e18b0248a50f1a9cf78
Brandon McGarvey <brandon.mcgarvey@outlook.com>
2022-03-12 13:57:48 -0500

    Wallet address returned to HTML

--- BlazorWebAssembly/wwwroot/connectEthers.js
+++ BlazorWebAssembly/wwwroot/connectEthers.js
@@ -1,6 +1,9 @@
+import { ethers } from "./ethers-5.1.esm.min.js";
+const provider = new ethers.providers.Web3Provider(window.ethereum)
 
+await provider.send("eth_requestAccounts", []);
 
-
-function test2() {
-    document.getElementById('hello').innerHTML = "Goodbye";            
-    }
+const signer = provider.getSigner();
+var address = await signer.getAddress();
+document.getElementById('address').innerHTML =
+    "Address: " + await signer.getAddress();
