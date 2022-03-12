36b42a8f74949b629cb99ac03b4110e53622114e
Brandon McGarvey <brandon.mcgarvey@outlook.com>
2022-03-12 10:15:14 -0500

    comments

--- BlazorWebAssembly/wwwroot/connectEthers.js
+++ BlazorWebAssembly/wwwroot/connectEthers.js
@@ -1,11 +1,11 @@
 
-/*import {ethers} from "./ethers-5.1.esm.min.js";
+import {ethers} from "./ethers-5.1.esm.min.js";
 const provider = new ethers.providers.Web3Provider(window.ethereum)
 
 await provider.send("eth_requestAccounts", []);
 
 const signer = provider.getSigner();
-var address = await signer.getAddress();*/
+var address = await signer.getAddress();
 
 function test2() {
     document.getElementById('hello').innerHTML = "Goodbye";            
