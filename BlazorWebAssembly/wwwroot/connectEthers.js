4848661367ea9f4cc203ea1d35b896bf7253964d
Brandon McGarvey <brandon.mcgarvey@outlook.com>
2022-03-12 10:06:02 -0500

    Add project files.

---
+++ BlazorWebAssembly/wwwroot/connectEthers.js
@@ -0,0 +1,12 @@
+
+/*import {ethers} from "./ethers-5.1.esm.min.js";
+const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+await provider.send("eth_requestAccounts", []);
+
+const signer = provider.getSigner();
+var address = await signer.getAddress();*/
+
+function test2() {
+    document.getElementById('hello').innerHTML = "Goodbye";            
+    }
